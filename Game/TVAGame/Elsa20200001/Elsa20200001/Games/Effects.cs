9e0b5bc1a3d10a7d27a242453ffe79bc70c36f1c
soleil.taruto (cerulean.charlotte) <soleil.taruto@gmail.com>
2022-05-04 01:50:27 +0900

    ss

--- Game/TVAGame/Elsa20200001/Elsa20200001/Games/Effects.cs
+++ Game/TVAGame/Elsa20200001/Elsa20200001/Games/Effects.cs
@@ -30,22 +30,7 @@ namespace Charlotte.Games
 			}
 		}
 
-		public static IEnumerable<bool> 跳ねた(double x, double y)
-		{
-			foreach (DDScene scene in DDSceneUtils.Create(5))
-			{
-				DDDraw.SetAlpha(0.7);
-				DDDraw.SetBright(1.0, 1.0, 0.0);
-				DDDraw.DrawBegin(Ground.I.Picture.WhiteCircle, x - DDGround.ICamera.X, y - DDGround.ICamera.Y);
-				DDDraw.DrawZoom(0.3 * scene.Rate);
-				DDDraw.DrawEnd();
-				DDDraw.Reset();
-
-				yield return true;
-			}
-		}
-
-		public static IEnumerable<bool> 小爆発(double x, double y)
+		public static IEnumerable<bool> 小爆発(double x, double y) // ★サンプルとしてキープ
 		{
 			foreach (DDScene scene in DDSceneUtils.Create(5))
 			{
@@ -60,14 +45,44 @@ namespace Charlotte.Games
 			}
 		}
 
-		public static IEnumerable<bool> 中爆発(double x, double y)
+		public static IEnumerable<bool> 中爆発(double x, double y) // ★サンプルとしてキープ
 		{
 			foreach (DDScene scene in DDSceneUtils.Create(10))
 			{
 				DDDraw.SetAlpha(0.7);
 				DDDraw.SetBright(1.0, 0.6, 0.3);
 				DDDraw.DrawBegin(Ground.I.Picture.WhiteCircle, x - DDGround.ICamera.X, y - DDGround.ICamera.Y);
+				DDDraw.DrawZoom(1.5 * scene.Rate);
+				DDDraw.DrawEnd();
+				DDDraw.Reset();
+
+				yield return true;
+			}
+		}
+
+		public static IEnumerable<bool> 大爆発(double x, double y) // ★サンプルとしてキープ
+		{
+			foreach (DDScene scene in DDSceneUtils.Create(10))
+			{
+				DDDraw.SetAlpha(0.7);
+				DDDraw.SetBright(0.6, 0.8, 1.0);
+				DDDraw.DrawBegin(Ground.I.Picture.WhiteCircle, x - DDGround.ICamera.X, y - DDGround.ICamera.Y);
 				DDDraw.DrawZoom(3.0 * scene.Rate);
+				DDDraw.DrawEnd();
+				DDDraw.Reset();
+
+				yield return true;
+			}
+		}
+
+		public static IEnumerable<bool> 跳ねた(double x, double y)
+		{
+			foreach (DDScene scene in DDSceneUtils.Create(5))
+			{
+				DDDraw.SetAlpha(0.7);
+				DDDraw.SetBright(1.0, 1.0, 0.0);
+				DDDraw.DrawBegin(Ground.I.Picture.WhiteCircle, x - DDGround.ICamera.X, y - DDGround.ICamera.Y);
+				DDDraw.DrawZoom(0.3 * scene.Rate);
 				DDDraw.DrawEnd();
 				DDDraw.Reset();
 
