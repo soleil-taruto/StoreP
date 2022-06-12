3a6d1e277372e690283dc9c849aba834e154dc60
soleil.taruto (cerulean.charlotte) <soleil.taruto@gmail.com>
2022-06-12 23:26:20 +0900

    ss

--- Dev/Program/Tools/HTTCmdP/Claes20200001/Claes20200001/Program.cs
+++ Dev/Program/Tools/HTTCmdP/Claes20200001/Claes20200001/Program.cs
@@ -255,6 +255,8 @@ namespace Charlotte
 
 				if (hostName != "localhost" && hostPortNo != 80) // localhostではないとき、ポート番号80以外は80へ飛ばす。★自宅サーバー特有の処理
 				{
+					ProcMain.WriteLog("ポート80へ転送(自宅サーバー特有の処理)");
+
 					channel.ResStatus = 301;
 					channel.ResHeaderPairs.Add(new string[] { "Location", "http://" + hostName + channel.PathQuery });
 					//channel.ResBody = null;
