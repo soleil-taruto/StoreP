a82c2bdb6de11141ac33258aa4f52c3a30ce62e2
soleil.taruto (cerulean.charlotte) <soleil.taruto@gmail.com>
2022-06-29 00:11:39 +0900

    ss

--- Dev/Game/GameJS/Shooting_v002/Gattonero20200001/Title.js
+++ Dev/Game/GameJS/Shooting_v002/Gattonero20200001/Title.js
@@ -53,9 +53,9 @@ function* <generatorForTask> TitleMain()
 		PrintRect(0, 0, Screen_W, Screen_H);
 
 		SetColor("#000000");
-		SetPrint(40, 320, 0);
-		SetFSize(160);
-		PrintLine("Template");
+		SetPrint(15, 320, 0);
+		SetFSize(180);
+		PrintLine("Shooting");
 
 		selectIndex = DrawSimpleMenu(selectIndex, 100, Screen_H - 300, 70, @@_Buttons.map(v => v.Text));
 
