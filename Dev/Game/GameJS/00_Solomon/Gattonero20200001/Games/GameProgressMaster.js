7e8911a312285467e343c9062f049150e3dca503
soleil.taruto (cerulean.charlotte) <soleil.taruto@gmail.com>
2022-06-29 16:35:15 +0900

    ss

--- Dev/Game/GameJS/00_Solomon/Gattonero20200001/Games/GameProgressMaster.js
+++ Dev/Game/GameJS/00_Solomon/Gattonero20200001/Games/GameProgressMaster.js
@@ -2,17 +2,20 @@
 	部屋(ステージ)選択メニュー
 */
 
-var<int> PANEL_L = 50; // 左上パネルの左座標
-var<int> PANEL_T = 50; // 左上パネルの上座標
-var<int> PANEL_W = 160;
-var<int> PANEL_H = 160;
-var<int> PANEL_X_GAP = 20;
-var<int> PANEL_Y_GAP = 20;
-var<int> PANEL_X_NUM = 4;
-var<int> PANEL_Y_NUM = 4;
+var<int> @@_PANEL_L = 50; // 左上パネルの左座標
+var<int> @@_PANEL_T = 50; // 左上パネルの上座標
+var<int> @@_PANEL_W = 160;
+var<int> @@_PANEL_H = 160;
+var<int> @@_PANEL_X_GAP = 20;
+var<int> @@_PANEL_Y_GAP = 20;
+var<int> @@_PANEL_X_NUM = 4;
+var<int> @@_PANEL_Y_NUM = 4;
 
 function* <generatorForTask> MapSelectMenu()
 {
+	var<int> selectX = 0;
+	var<int> selectY = 0;
+
 	FreezeInput();
 
 	for (; ; )
@@ -24,13 +27,13 @@ function* <generatorForTask> MapSelectMenu()
 
 			var<int> index = 0;
 
-			for (var<int> y = 0; y < PANEL_Y_NUM; y++)
-			for (var<int> x = 0; x < PANEL_X_NUM; x++)
+			for (var<int> y = 0; y < @@_PANEL_Y_NUM; y++)
+			for (var<int> x = 0; x < @@_PANEL_X_NUM; x++)
 			{
-				var<int> l = PANEL_L + x * (PANEL_W + PANEL_X_GAP);
-				var<int> t = PANEL_T + y * (PANEL_H + PANEL_Y_GAP);
-				var<int> w = PANEL_W;
-				var<int> h = PANEL_H;
+				var<int> l = @@_PANEL_L + x * (@@_PANEL_W + @@_PANEL_X_GAP);
+				var<int> t = @@_PANEL_T + y * (@@_PANEL_H + @@_PANEL_Y_GAP);
+				var<int> w = @@_PANEL_W;
+				var<int> h = @@_PANEL_H;
 
 				if (!IsOut(
 					CreateD2Point(mx, my),
@@ -42,8 +45,44 @@ function* <generatorForTask> MapSelectMenu()
 					yield* @@_Game(index);
 					FreezeInput();
 				}
+
+				index++;
 			}
 		}
+		if (IsPound(GetInput_2()))
+		{
+			selectY++;
+		}
+		if (IsPound(GetInput_4()))
+		{
+			selectX--;
+		}
+		if (IsPound(GetInput_6()))
+		{
+			selectX++;
+		}
+		if (IsPound(GetInput_8()))
+		{
+			selectY--;
+		}
+		if (IsPound(GetInput_A()))
+		{
+			var<int> index = selectX + selectY * @@_PANEL_Y_NUM;
+
+			FreezeInput();
+			yield* @@_Game(index);
+			FreezeInput();
+		}
+		if (IsPound(GetInput_B()))
+		{
+			break; // タイトルへ戻る
+		}
+
+		selectX += @@_PANEL_X_NUM;
+		selectX %= @@_PANEL_X_NUM;
+
+		selectY += @@_PANEL_Y_NUM;
+		selectY %= @@_PANEL_Y_NUM;
 
 		// 描画ここから
 
@@ -52,16 +91,26 @@ function* <generatorForTask> MapSelectMenu()
 
 		var<int> index = 0;
 
-		for (var<int> y = 0; y < PANEL_Y_NUM; y++)
-		for (var<int> x = 0; x < PANEL_X_NUM; x++)
+		for (var<int> y = 0; y < @@_PANEL_Y_NUM; y++)
+		for (var<int> x = 0; x < @@_PANEL_X_NUM; x++)
 		{
-			var<int> l = PANEL_L + x * (PANEL_W + PANEL_X_GAP);
-			var<int> t = PANEL_T + y * (PANEL_H + PANEL_Y_GAP);
+			var<int> l = @@_PANEL_L + x * (@@_PANEL_W + @@_PANEL_X_GAP);
+			var<int> t = @@_PANEL_T + y * (@@_PANEL_H + @@_PANEL_Y_GAP);
 
-			SetColor("#ffffff");
-			PrintRect(l, t, PANEL_W, PANEL_H);
+			if (
+				x == selectX &&
+				y == selectY
+				)
+			{
+				SetColor("#ffff00");
+			}
+			else
+			{
+				SetColor("#ffffff");
+			}
+			PrintRect(l, t, @@_PANEL_W, @@_PANEL_H);
 			SetColor("#000000");
-			SetPrint(l + 30, t + 100, 0);
+			SetPrint(l + 30, t + 110, 0);
 			SetFSize(80);
 			PrintLine(ZPad(index + 1, 2, "0"));
 
