0af33cc2e695c2d0d3638e45c5b83e2c86d7ffb2
soleil.taruto (cerulean.charlotte) <soleil.taruto@gmail.com>
2022-07-01 00:22:31 +0900

    ss

--- Dev/Program/Test0001/Claes20200001/Claes20200001/Tests/Test0001.cs
+++ Dev/Program/Test0001/Claes20200001/Claes20200001/Tests/Test0001.cs
@@ -11,7 +11,24 @@ namespace Charlotte.Tests
 	{
 		public void Test01()
 		{
-			// none
+			Console.WriteLine(string.Join(", ",
+				SCommon.InsertRange(new int[] { 1, 2, 3, 4 }, 2, new int[] { 9, 8, 7 }))); // 1, 2, 9, 8, 7, 3, 4
+			Console.WriteLine(string.Join(", ",
+				SCommon.InsertRange(new int[] { 1, 2, 3 }, 0, new int[] { 999 }))); // 999, 1, 2, 3
+			Console.WriteLine(string.Join(", ",
+				SCommon.InsertRange(new int[] { 1, 2, 3 }, 3, new int[] { 999 }))); // 1, 2, 3, 999
+			Console.WriteLine(string.Join(", ",
+				SCommon.InsertRange(new int[] { 1, 2, 3 }, 0, new int[] { 7, 8, 9 }))); // 7, 8, 9, 1, 2, 3
+			Console.WriteLine(string.Join(", ",
+				SCommon.InsertRange(new int[] { 1, 2, 3 }, 3, new int[] { 7, 8, 9 }))); // 1, 2, 3, 7, 8, 9
+			Console.WriteLine(string.Join(", ",
+				SCommon.InsertRange(new int[] { }, 0, new int[] { 99, 98, 97 }))); // 99, 98, 97
+			Console.WriteLine(string.Join(", ",
+				SCommon.InsertRange(new int[] { 99, 98, 97 }, 0, new int[] { }))); // 99, 98, 97
+			Console.WriteLine(string.Join(", ",
+				SCommon.InsertRange(new int[] { 99, 98, 97 }, 3, new int[] { }))); // 99, 98, 97
+			Console.WriteLine(string.Join(", ",
+				SCommon.InsertRange(new int[] { 1, 2, 3, 4 }, 2, new int[] { }))); // 1, 2, 3, 4
 		}
 	}
 }
