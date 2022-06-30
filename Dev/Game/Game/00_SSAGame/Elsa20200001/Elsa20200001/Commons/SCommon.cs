046a8493da21bba1778ba462ad420340beaea7df
soleil.taruto (cerulean.charlotte) <soleil.taruto@gmail.com>
2022-06-30 20:52:17 +0900

    ss

--- Dev/Game/Game/00_SSAGame/Elsa20200001/Elsa20200001/Commons/SCommon.cs
+++ Dev/Game/Game/00_SSAGame/Elsa20200001/Elsa20200001/Commons/SCommon.cs
@@ -2599,5 +2599,63 @@ namespace Charlotte.Commons
 			Console.WriteLine(ToThrow(routine));
 			Console.WriteLine("★★★想定された例外のため処理を続行します。");
 		}
+
+		#region GetOutputDir
+
+		private static string GOD_Dir;
+
+		public static string GetOutputDir()
+		{
+			if (GOD_Dir == null)
+				GOD_Dir = GetOutputDir_Main();
+
+			return GOD_Dir;
+		}
+
+		private static string GetOutputDir_Main()
+		{
+			for (int c = 1; c <= 999; c++)
+			{
+				string dir = "C:\\" + c;
+
+				if (
+					!Directory.Exists(dir) &&
+					!File.Exists(dir)
+					)
+				{
+					SCommon.CreateDir(dir);
+					return dir;
+				}
+			}
+			throw new Exception("C:\\1 ～ 999 は使用できません。");
+		}
+
+		public static void OpenOutputDir()
+		{
+			SCommon.Batch(new string[] { "START " + GetOutputDir() });
+		}
+
+		public static void OpenOutputDirIfCreated()
+		{
+			if (GOD_Dir != null)
+			{
+				OpenOutputDir();
+			}
+		}
+
+		private static int NOP_Count = 0;
+
+		public static string NextOutputPath()
+		{
+			return Path.Combine(GetOutputDir(), (++NOP_Count).ToString("D4"));
+		}
+
+		#endregion
+
+		public static void Pause()
+		{
+			Console.WriteLine("Press ENTER key.");
+			Console.ReadLine();
+		}
 	}
 }
