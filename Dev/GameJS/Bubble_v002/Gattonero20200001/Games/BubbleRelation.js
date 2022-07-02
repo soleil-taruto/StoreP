bb338db4d462243740bab720338aeb88ed9ee7cf
soleil.taruto (cerulean.charlotte) <soleil.taruto@gmail.com>
2022-07-02 21:54:36 +0900

    ss

--- Dev/GameJS/Bubble_v002/Gattonero20200001/Games/BubbleRelation.js
+++ Dev/GameJS/Bubble_v002/Gattonero20200001/Games/BubbleRelation.js
@@ -11,17 +11,15 @@
 
 var<double> @@_EXPLODE_R = 45.0;
 
-var<int> @@_Busy = 0; // 0 == 暇, 1 == 処理中, 2 == コンボ中
+var<boolean> @@_Busy = false;
 
-function <void> BubbleRelation_着弾による爆発(<Enemy_t[]> enemies, <Enemy_t> baseEnemy, <Enemy_t> 着弾先_enemy)
+function <void> BubbleRelation_着弾による爆発(<Enemy_t[]> enemies, <Enemy_t> baseEnemy)
 {
-	var<boolean> comboMode = false;
-
 	enemies = CloneArray(enemies);
 
 	AddEffect(function* ()
 	{
-		while (@@_Busy == 2)
+		while (@@_Busy)
 		{
 			yield 1;
 		}
@@ -45,7 +43,7 @@ function <void> BubbleRelation_着弾による爆発(<Enemy_t[]> enemies, <Enemy_t> bas
 				if (enemy.@@_Reached == 1)
 				{
 					enemy.@@_Reached = 2;
-					@@_Extend(enemies, enemy, baseEnemy.Color);
+					@@_Extend(enemies, enemy);
 					extended = true;
 					yield 1;
 				}
@@ -69,38 +67,34 @@ function <void> BubbleRelation_着弾による爆発(<Enemy_t[]> enemies, <Enemy_t> bas
 				yield 1;
 			}
 
-			if (!@@_Combo) // ? コンボ発動中ではない -> 爆破する。
+			for (var<Enemy_t> enemy of enemies)
 			{
-				for (var<Enemy_t> enemy of enemies)
+				if (enemy.@@_Reached == 2)
 				{
-					if (enemy.@@_Reached == 2)
-					{
-						KillEnemy(enemy);
+					KillEnemy(enemy);
 
-						/*
-						for (var<int> w = 0; w < 5; w++) // ウェイト
-						{
-							yield 1;
-						}
-						*/
+					/*
+					for (var<int> w = 0; w < 5; w++) // ウェイト
+					{
 						yield 1;
 					}
+					*/
+					yield 1;
 				}
 			}
 		}
 
 		@@_Busy = false;
-		@@_Combo = false;
 	}());
 }
 
-function <void> @@_Extend(<Enemy_t[]> enemies, <Enemy_t> baseEnemy, <BallColor_e> targetColor)
+function <void> @@_Extend(<Enemy_t[]> enemies, <Enemy_t> baseEnemy)
 {
 	for (var<Enemy_t> enemy of enemies)
 	{
 		if (
 			enemy.@@_Reached == 0 &&
-			enemy.Color == targetColor &&
+			enemy.Color == baseEnemy.Color && // 同じ色のみ伝搬する。
 			Math.abs(enemy.X - baseEnemy.X) < @@_EXPLODE_R &&
 			Math.abs(enemy.Y - baseEnemy.Y) < @@_EXPLODE_R &&
 			GetDistance(enemy.X - baseEnemy.X, enemy.Y - baseEnemy.Y) < @@_EXPLODE_R
