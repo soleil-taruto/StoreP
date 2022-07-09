9abd219232fb31d2ba3fb89b2ddbea3327a412ee
soleil.taruto (cerulean.charlotte) <soleil.taruto@gmail.com>
2022-07-10 03:53:00 +0900

    ss

---
+++ Dev/Program/JSJoin/Claes20200001/Claes20200001/JSConfusers/JSResource.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using Charlotte.Commons;
+
+namespace Charlotte.JSConfusers
+{
+	public class JSResource
+	{
+		#region 予約語リスト
+
+		private static string RES_予約語リスト = Common.ReadResTextFile("RES_予約語リスト.txt");
+
+		#endregion
+
+		// ======
+		// ======
+		// ======
+
+		public static string[] 予約語リスト = SCommon.TextToLines(RES_予約語リスト)
+			.Select(v => v.Trim())
+			.Where(v => v != "" && v[0] != ';') // 空行とコメント行を除去
+			.Distinct()
+			.ToArray();
+	}
+}
