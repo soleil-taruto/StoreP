4c8ae6244b445e1387488ae2e4bf6339e45f4002
soleil.taruto (cerulean.charlotte) <soleil.taruto@gmail.com>
2022-07-17 17:07:05 +0900

    ss

--- Dev/GameJS/Hako/Gattonero20200001/Games/LocalStorage.js
+++ Dev/GameJS/Hako/Gattonero20200001/Games/LocalStorage.js
@@ -12,13 +12,8 @@ function <void> LoadLocalStorage()
 		{
 			throw null;
 		}
-		data = Tokenize(@@_Decode(data), ";", false, false);
+		data = Tokenize(data, ";", false, false);
 		var<int> c = 0;
-
-		if (data[c++] != "@(APID)_Credentials")
-		{
-			throw null;
-		}
 
 		// SaveData >
 
@@ -44,7 +39,7 @@ function <void> LoadLocalStorage()
 
 function <void> SaveLocalStorage()
 {
-	var<string[]> data = [ "@(APID)_Credentials" ];
+	var<string[]> data = [];
 
 	// SaveData >
 
@@ -55,60 +50,5 @@ function <void> SaveLocalStorage()
 
 	// < SaveData
 
-	SetLocalStorageValue("@(APID)_SaveData", @@_Encode(data.join(";")));
-}
-
-function <string> @@_Encode(<string> data)
-{
-	data = window.btoa(data);
-	data = @@_UnaddEndEq(data);
-	data = @@_Shuffle(data);
-
-	return data;
-}
-
-function <string> @@_Decode(<string> data)
-{
-	data = @@_Shuffle(data);
-	data = @@_AddEndEq(data);
-	data = window.atob(data);
-
-	return data;
-}
-
-function <string> @@_Shuffle(<string> data)
-{
-	var<char[]> chrs = [ ... data ];
-
-	var<int> l = 0;
-	var<int> r = chrs.length - 1;
-
-	while(l < r)
-	{
-		var tmp = chrs[l];
-		chrs[l] = chrs[r];
-		chrs[r] = tmp;
-
-		l++;
-		r -= 3;
-	}
-	return chrs.join("");
-}
-
-function <string> @@_AddEndEq(<string> data)
-{
-	while (data.length % 4 != 0)
-	{
-		data += "=";
-	}
-	return data;
-}
-
-function <string> @@_UnaddEndEq(<string> data)
-{
-	while (data.endsWith("="))
-	{
-		data = data.substring(0, data.length - 1);
-	}
-	return data;
+	SetLocalStorageValue("@(APID)_SaveData", data.join(";"));
 }
