7e736a249e4ef5df2a9cdc4fda751456bb6f51fe
soleil.taruto (cerulean.charlotte) <soleil.taruto@gmail.com>
2022-07-20 22:50:45 +0900

    ss

--- Dev/Game/00_MSSAGame/Elsa20200001/Elsa20200001/GameCommons/DDSE.cs
+++ Dev/Game/00_MSSAGame/Elsa20200001/Elsa20200001/GameCommons/DDSE.cs
@@ -7,7 +7,7 @@ namespace Charlotte.GameCommons
 {
 	public class DDSE
 	{
-		public bool Globally = true; // 廃止予定
+		public bool Globally = false; // 廃止予定
 		public bool Locally { get { return !this.Globally; } } // 廃止予定
 		public DDSound Sound;
 		public double Volume = 0.5; // 廃止予定
@@ -29,19 +29,6 @@ namespace Charlotte.GameCommons
 			DDSEUtils.Add(this);
 		}
 
-		/// <summary>
-		/// 廃止予定
-		/// ローカル化する。
-		/// 初期化時に呼び出すこと。
-		/// -- 例：DDSE xxx = new DDSE("xxx.mp3").SetLocally();
-		/// </summary>
-		/// <returns>このインスタンス</returns>
-		public DDSE SetLocally()
-		{
-			this.Globally = false;
-			return this;
-		}
-
 		public void Play(bool once = true)
 		{
 			if (once)
@@ -53,17 +40,6 @@ namespace Charlotte.GameCommons
 		public void Stop()
 		{
 			DDSEUtils.Stop(this);
-		}
-
-		/// <summary>
-		/// 廃止予定
-		/// 固有の音量を設定する。
-		/// </summary>
-		/// <param name="volume">音量</param>
-		public void SetVolume(double volume)
-		{
-			this.Volume = volume;
-			this.UpdateVolume();
 		}
 
 		public void UpdateVolume()
