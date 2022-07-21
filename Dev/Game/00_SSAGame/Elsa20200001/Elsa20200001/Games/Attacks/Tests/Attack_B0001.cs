c67bde67d81a448640cb6cf0aa64cafffd9ea680
soleil.taruto (cerulean.charlotte) <soleil.taruto@gmail.com>
2022-07-22 03:54:55 +0900

    ss

--- Dev/Game/00_SSAGame/Elsa20200001/Elsa20200001/Games/Attacks/Tests/Attack_B0001.cs
+++ Dev/Game/00_SSAGame/Elsa20200001/Elsa20200001/Games/Attacks/Tests/Attack_B0001.cs
@@ -34,8 +34,8 @@ namespace Charlotte.Games.Attacks.Tests
 				DDGround.EL.Add(() =>
 				{
 					DDPrint.SetDebug(
-						(int)Game.I.Player.X - DDGround.ICamera.X - 20,
-						(int)Game.I.Player.Y - DDGround.ICamera.Y - 50
+						(int)Game.I.Player.X - DDGround.ICamera.X - 80,
+						(int)Game.I.Player.Y - DDGround.ICamera.Y - 60
 						);
 					DDPrint.SetBorder(new I3Color(0, 0, 192));
 					DDPrint.Print("Attack_B0001 テスト");
@@ -46,7 +46,7 @@ namespace Charlotte.Games.Attacks.Tests
 
 				DDDraw.SetTaskList(Game.I.Player.Draw_EL);
 				DDDraw.DrawCenter(
-					Ground.I.Picture.Cirno_01,
+					Ground.I.Picture2.Tewi_立ち[0],
 					Game.I.Player.X - DDGround.ICamera.X,
 					Game.I.Player.Y - DDGround.ICamera.Y
 					);
