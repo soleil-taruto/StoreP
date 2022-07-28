fcdc8bf810aec6231f594f48a8a55d605ad1a1ae
soleil.taruto (cerulean.charlotte) <soleil.taruto@gmail.com>
2022-07-29 03:29:09 +0900

    ss

--- Dev/GameJS/Doremy/Gattonero20200001/GameCommons/Resource_Picture.js
+++ Dev/GameJS/Doremy/Gattonero20200001/GameCommons/Resource_Picture.js
@@ -38,45 +38,171 @@ var<Image> P_WhiteCircle = @@_Load(Resources.General__WhiteCircle_png);
 var<Image> P_ExplodePiece = @@_Load(Resources.Picture__光る星20_png);
 var<Image> P_Goal = @@_Load(Resources.Picture__Goal_png);
 
+// ==========
 // プレイヤー
+// ==========
 
-var<Image> P_PlayerAttack = @@_Load(Resources.えむくろ__CirnoAttack_png);
-var<Image> P_PlayerStand  = @@_Load(Resources.えむくろ__CirnoStand_png);
-var<Image> P_PlayerJump   = @@_Load(Resources.えむくろ__CirnoJump_png);
+var<Image> P_PlayerJump       = @@_Load(Resources.ロックマン風__Doremy__chara_a01_jump01_png);
+var<Image> P_PlayerJumpAttack = @@_Load(Resources.ロックマン風__Doremy__chara_a01_jump02attack_png);
+var<Image> P_PlayerJumpDamage = @@_Load(Resources.ロックマン風__Doremy__chara_a01_jump03damage_png);
 
 var<Image[]> P_PlayerRun =
 [
-	@@_Load(Resources.えむくろ__CirnoRun_001_png),
-	@@_Load(Resources.えむくろ__CirnoRun_002_png),
-	@@_Load(Resources.えむくろ__CirnoRun_003_png),
-	@@_Load(Resources.えむくろ__CirnoRun_004_png),
+	@@_Load(Resources.ロックマン風__Doremy__chara_a01_run01_png),
+	@@_Load(Resources.ロックマン風__Doremy__chara_a01_run02_png),
+	@@_Load(Resources.ロックマン風__Doremy__chara_a01_run03_png),
 ];
 
-// プレイヤー_Mirror
+var<Image[]> P_PlayerRunAttack =
+[
+	@@_Load(Resources.ロックマン風__Doremy__chara_a01_run04_attack_png),
+	@@_Load(Resources.ロックマン風__Doremy__chara_a01_run05_attack_png),
+	@@_Load(Resources.ロックマン風__Doremy__chara_a01_run06_attack_png),
+];
 
-var<Image> P_PlayerMirrorAttack = @@_Load(Resources.えむくろ__Mirror__CirnoAttack_png);
-var<Image> P_PlayerMirrorStand  = @@_Load(Resources.えむくろ__Mirror__CirnoStand_png);
-var<Image> P_PlayerMirrorJump   = @@_Load(Resources.えむくろ__Mirror__CirnoJump_png);
+var<Image> P_PlayerSliding      = @@_Load(Resources.ロックマン風__Doremy__chara_a01_sliding01_png);
+var<Image> P_PlayerTelepo01     = @@_Load(Resources.ロックマン風__Doremy__chara_a01_telepo01_png);
+var<Image> P_PlayerTelepo02     = @@_Load(Resources.ロックマン風__Doremy__chara_a01_telepo02_png);
+var<Image> P_PlayerTelepo03     = @@_Load(Resources.ロックマン風__Doremy__chara_a01_telepo03_png);
+var<Image> P_PlayerWait         = @@_Load(Resources.ロックマン風__Doremy__chara_a01_wait01_png);
+var<Image> P_PlayerWaitMabataki = @@_Load(Resources.ロックマン風__Doremy__chara_a01_wait02_png);
+var<Image> P_PlayerWaitStart    = @@_Load(Resources.ロックマン風__Doremy__chara_a01_wait03start_png);
+var<Image> P_PlayerWaitAttack   = @@_Load(Resources.ロックマン風__Doremy__chara_a01_wait04attack_png);
+
+var<Image[]> P_PlayerClimb =
+[
+	@@_Load(Resources.ロックマン風__Doremy__chara_a01_climb01_png),
+	@@_Load(Resources.ロックマン風__Doremy__chara_a01_climb02_png),
+];
+
+var<Image> P_PlayerClimbAttack = @@_Load(Resources.ロックマン風__Doremy__chara_a01_climb03_attack_png);
+var<Image> P_PlayerClimbTop    = @@_Load(Resources.ロックマン風__Doremy__chara_a01_climb04_png);
+
+var<Image> P_PlayerEffectShockA = @@_Load(Resources.ロックマン風__Doremy__effect_a01_shock01_png);
+var<Image> P_PlayerEffectShockB =
+[
+	@@_Load(Resources.ロックマン風__Doremy__effect_a01_shock02_png),
+	@@_Load(Resources.ロックマン風__Doremy__effect_a01_shock03_png),
+	@@_Load(Resources.ロックマン風__Doremy__effect_a01_shock04_png),
+];
+
+var<Image> P_PlayerEffectSliding =
+[
+	@@_Load(Resources.ロックマン風__Doremy__effect_a01_sliding01_png),
+	@@_Load(Resources.ロックマン風__Doremy__effect_a01_sliding02_png),
+	@@_Load(Resources.ロックマン風__Doremy__effect_a01_sliding03_png),
+];
+
+// =================
+// プレイヤー Mirror
+// =================
+
+var<Image> P_PlayerMirrorJump       = @@_Load(Resources.ロックマン風__Doremy__Mirror__chara_a01_jump01_png);
+var<Image> P_PlayerMirrorJumpAttack = @@_Load(Resources.ロックマン風__Doremy__Mirror__chara_a01_jump02attack_png);
+var<Image> P_PlayerMirrorJumpDamage = @@_Load(Resources.ロックマン風__Doremy__Mirror__chara_a01_jump03damage_png);
 
 var<Image[]> P_PlayerMirrorRun =
 [
-	@@_Load(Resources.えむくろ__Mirror__CirnoRun_001_png),
-	@@_Load(Resources.えむくろ__Mirror__CirnoRun_002_png),
-	@@_Load(Resources.えむくろ__Mirror__CirnoRun_003_png),
-	@@_Load(Resources.えむくろ__Mirror__CirnoRun_004_png),
+	@@_Load(Resources.ロックマン風__Doremy__Mirror__chara_a01_run01_png),
+	@@_Load(Resources.ロックマン風__Doremy__Mirror__chara_a01_run02_png),
+	@@_Load(Resources.ロックマン風__Doremy__Mirror__chara_a01_run03_png),
 ];
 
-// プレイヤー_End
-
-var<Image> P_TileNone = @@_Load(Resources.Tile__Tile_None_png);
-
-var<Image[]> P_Tiles =
+var<Image[]> P_PlayerMirrorRunAttack =
 [
-	@@_Load(Resources.Tile__Tile_B0001_png),
-	@@_Load(Resources.Tile__Tile_B0002_png),
-	@@_Load(Resources.Tile__Tile_B0003_png),
-	@@_Load(Resources.Tile__Tile_B0004_png),
+	@@_Load(Resources.ロックマン風__Doremy__Mirror__chara_a01_run04_attack_png),
+	@@_Load(Resources.ロックマン風__Doremy__Mirror__chara_a01_run05_attack_png),
+	@@_Load(Resources.ロックマン風__Doremy__Mirror__chara_a01_run06_attack_png),
 ];
+
+var<Image> P_PlayerMirrorSliding      = @@_Load(Resources.ロックマン風__Doremy__Mirror__chara_a01_sliding01_png);
+var<Image> P_PlayerMirrorTelepo01     = @@_Load(Resources.ロックマン風__Doremy__Mirror__chara_a01_telepo01_png);
+var<Image> P_PlayerMirrorTelepo02     = @@_Load(Resources.ロックマン風__Doremy__Mirror__chara_a01_telepo02_png);
+var<Image> P_PlayerMirrorTelepo03     = @@_Load(Resources.ロックマン風__Doremy__Mirror__chara_a01_telepo03_png);
+var<Image> P_PlayerMirrorWait         = @@_Load(Resources.ロックマン風__Doremy__Mirror__chara_a01_wait01_png);
+var<Image> P_PlayerMirrorWaitMabataki = @@_Load(Resources.ロックマン風__Doremy__Mirror__chara_a01_wait02_png);
+var<Image> P_PlayerMirrorWaitStart    = @@_Load(Resources.ロックマン風__Doremy__Mirror__chara_a01_wait03start_png);
+var<Image> P_PlayerMirrorWaitAttack   = @@_Load(Resources.ロックマン風__Doremy__Mirror__chara_a01_wait04attack_png);
+
+var<Image[]> P_PlayerMirrorClimb =
+[
+	@@_Load(Resources.ロックマン風__Doremy__Mirror__chara_a01_climb01_png),
+	@@_Load(Resources.ロックマン風__Doremy__Mirror__chara_a01_climb02_png),
+];
+
+var<Image> P_PlayerMirrorClimbAttack = @@_Load(Resources.ロックマン風__Doremy__Mirror__chara_a01_climb03_attack_png);
+var<Image> P_PlayerMirrorClimbTop    = @@_Load(Resources.ロックマン風__Doremy__Mirror__chara_a01_climb04_png);
+
+var<Image> P_PlayerMirrorEffectShockA = @@_Load(Resources.ロックマン風__Doremy__Mirror__effect_a01_shock01_png);
+var<Image> P_PlayerMirrorEffectShockB =
+[
+	@@_Load(Resources.ロックマン風__Doremy__Mirror__effect_a01_shock02_png),
+	@@_Load(Resources.ロックマン風__Doremy__Mirror__effect_a01_shock03_png),
+	@@_Load(Resources.ロックマン風__Doremy__Mirror__effect_a01_shock04_png),
+];
+
+var<Image> P_PlayerMirrorEffectSliding =
+[
+	@@_Load(Resources.ロックマン風__Doremy__Mirror__effect_a01_sliding01_png),
+	@@_Load(Resources.ロックマン風__Doremy__Mirror__effect_a01_sliding02_png),
+	@@_Load(Resources.ロックマン風__Doremy__Mirror__effect_a01_sliding03_png),
+];
+
+// ==============
+// プレイヤー End
+// ==============
+
+// ======
+// タイル
+// ======
+
+var<Image[]> P_TileNone = @@_Load(Resources.Tile__Tile_B0001_png);
+
+var<Image[]> P_Tile_B1 = @@_Load(Resources.Tile__Tile_B0001_png);
+var<Image[]> P_Tile_B2 = @@_Load(Resources.Tile__Tile_B0002_png);
+var<Image[]> P_Tile_B3 = @@_Load(Resources.Tile__Tile_B0003_png);
+var<Image[]> P_Tile_B4 = @@_Load(Resources.Tile__Tile_B0004_png);
+
+/*
+	地面
+*/
+var<Image> P_Tile_Ground1 = @@_Load(Resources.ロックマン風__Tile__stage01_chip_a01_png);
+var<Image> P_Tile_Ground2 = @@_Load(Resources.ロックマン風__Tile__stage01_chip_a02_png);
+
+/*
+	フェンス
+	番号はテンキー式の配置
+*/
+var<Image> P_Tile_Fence1 = @@_Load(Resources.stage02_chip_b05stage02_chip_b01_png);
+var<Image> P_Tile_Fence2 = @@_Load(Resources.stage02_chip_b05stage02_chip_b08_png);
+var<Image> P_Tile_Fence3 = @@_Load(Resources.stage02_chip_b05stage02_chip_b07_png);
+var<Image> P_Tile_Fence4 = @@_Load(Resources.stage02_chip_b05stage02_chip_b02_png);
+var<Image> P_Tile_Fence5 = @@_Load(Resources.stage02_chip_b05stage02_chip_b09_png);
+var<Image> P_Tile_Fence6 = @@_Load(Resources.stage02_chip_b05stage02_chip_b06_png);
+var<Image> P_Tile_Fence7 = @@_Load(Resources.stage02_chip_b05stage02_chip_b03_png);
+var<Image> P_Tile_Fence8 = @@_Load(Resources.stage02_chip_b05stage02_chip_b04_png);
+var<Image> P_Tile_Fence9 = @@_Load(Resources.stage02_chip_b05stage02_chip_b05_png);
+
+/*
+	煉瓦(小)
+*/
+var<Image> P_Tile_Brick_S = @@_Load(Resources.ロックマン風__Tile__stage02_chip_e01_png);
+
+/*
+	煉瓦(大)
+*/
+var<Image> P_Tile_Brick_L1 = @@_Load(Resources.ロックマン風__Tile__stage02_chip_c01_png);
+var<Image> P_Tile_Brick_L2 = @@_Load(Resources.ロックマン風__Tile__stage02_chip_c02_png);
+var<Image> P_Tile_Brick_L3 = @@_Load(Resources.ロックマン風__Tile__stage02_chip_c03_png);
+
+/*
+	梯子
+*/
+var<Image> P_Tile_Ladder = @@_Load(Resources.ロックマン風__Tile__stage02_chip_d01_png);
+
+// ==========
+// タイル End
+// ==========
 
 var<Image> P_Wall0001 = @@_Load(Resources.素材Good__Wall_0001_png);
 var<Image> P_Wall0002 = @@_Load(Resources.素材Good__Wall_0002_png);
