51f14b090f4ce19bc17d38908a02f34132d56fff
soleil.taruto (cerulean.charlotte) <soleil.taruto@gmail.com>
2022-08-01 05:33:23 +0900

    ss

--- Dev/GameJS/Doremy/Gattonero20200001/Games/Game.js
+++ Dev/GameJS/Doremy/Gattonero20200001/Games/Game.js
@@ -505,30 +505,66 @@ function <void> @@_DrawFront()
 */
 function* <generatorForTask> @@_StartMotion()
 {
-	for (var<Scene_t> scene of CreateScene(60))
+	for (var<Scene_t> scene of CreateScene(20))
 	{
 		@@_カメラ位置調整(false);
 
 		@@_DrawWall();
 		@@_DrawMap();
 
-		for (var<int> c = 0; c < 4; c++)
+		yield 1;
+	}
+
+	var<double> destX = PlayerX;
+	var<double> destY = PlayerY;
+
+	destY -= 8; // 接地したとき上に押し出される距離
+
+	var<double> x = destX;
+	var<double> y = Math.min(destY - Screen_H / 2.0, Camera.Y);
+	var<double> yAdd = 3.0; // 初速度
+
+	for (; ; )
+	{
+		@@_カメラ位置調整(false);
+
+		y += yAdd;
+		yAdd += 1.0; // 加速
+
+		if (destY < y)
 		{
-			var<double> dx = PlayerX - Camera.X;
-			var<double> dy = PlayerY - Camera.Y;
-
-			dy -= 14; // 接地したとき上に押し出される距離
-
-			var<D2Point_t> pt = AngleToPoint(
-				scene.RemRate * scene.RemRate * 10.0 + (Math.PI / 2.0) * c,
-				scene.RemRate * scene.RemRate * 500.0
-				);
-
-			dx += pt.X;
-			dy += pt.Y;
-
-			Draw(P_PlayerWait, dx, dy, 0.5, 0.0, 1.0 + scene.RemRate * 2.0);
+			break;
 		}
+
+		@@_DrawWall();
+		@@_DrawMap();
+
+		Draw(P_PlayerTelepo01, x - Camera.X, y - Camera.Y, 1.0, 0.0, 1.0);
+
+		yield 1;
+	}
+
+	y = destY;
+
+	for (var<int> c = 0; c < 9; c++)
+	{
+		@@_カメラ位置調整(false);
+
+		var<Image> picture;
+
+		if (ToFix(c / 3) % 2 == 0)
+		{
+			picture = P_PlayerTelepo02;
+		}
+		else
+		{
+			picture = P_PlayerTelepo03;
+		}
+
+		@@_DrawWall();
+		@@_DrawMap();
+
+		Draw(picture, x - Camera.X, y - Camera.Y, 1.0, 0.0, 1.0);
 
 		yield 1;
 	}
@@ -589,39 +625,48 @@ function* <generatorForTask> @@_GoalMotion()
 
 	SE(S_Clear);
 
-	for (var<int> c = 0; c < 50; c++)
+	var<double> x = PlayerX;
+	var<double> y = PlayerY;
+
+	for (var<int> c = 0; c < 9; c++)
 	{
-		AddEffect(function* <generatorForTask> ()
+		var<Image> picture;
+
+		if (ToFix(c / 3) % 2 == 0)
 		{
-			var<double> x = PlayerX;
-			var<double> y = PlayerY;
-			var<D2Point_t> speed = AngleToPoint(Math.PI * 2.0 * GetRand1(), GetRand3(5.0, 15.0));
+			picture = P_PlayerTelepo02;
+		}
+		else
+		{
+			picture = P_PlayerTelepo03;
+		}
 
-			var<double> r = Math.PI * 2.0 * GetRand1();
-			var<double> rAdd = 0.3 * GetRand2();
-
-			for (; ; )
-			{
-				x += speed.X;
-				y += speed.Y;
-				r += rAdd;
-
-				if (IsOutOfCamera(CreateD2Point(x, y), 50.0))
-				{
-					break;
-				}
-
-				Draw(P_PlayerWait, x - Camera.X, y - Camera.Y, 0.7, r, 2.0);
-
-				yield 1;
-			}
-		}());
-	}
-
-	for (var<Scene_t> scene of CreateScene(60))
-	{
 		@@_DrawWall();
 		@@_DrawMap();
+
+		Draw(picture, x - Camera.X, y - Camera.Y, 1.0, 0.0, 1.0);
+
+		yield 1;
+	}
+
+	var<double> destX = x;
+	var<double> destY = Math.min(y - Screen_H / 2.0, Camera.Y);
+	var<double> yAdd = -3.0; // 初速度
+
+	for (; ; )
+	{
+		y += yAdd;
+		yAdd -= 1.0; // 加速
+
+		if (y < destY)
+		{
+			break;
+		}
+
+		@@_DrawWall();
+		@@_DrawMap();
+
+		Draw(P_PlayerTelepo01, x - Camera.X, y - Camera.Y, 1.0, 0.0, 1.0);
 
 		yield 1;
 	}
