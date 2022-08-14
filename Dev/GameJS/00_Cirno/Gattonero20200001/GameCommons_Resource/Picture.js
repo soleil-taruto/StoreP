7c2362f79aafdb7329c18147f155406dd84da735
soleil.taruto (cerulean.charlotte) <soleil.taruto@gmail.com>
2022-08-15 02:54:45 +0900

    ss

--- Dev/GameJS/00_Cirno/Gattonero20200001/GameCommons_Resource/Picture.js
+++ Dev/GameJS/00_Cirno/Gattonero20200001/GameCommons_Resource/Picture.js
@@ -18,6 +18,7 @@ var<Picture_t> P_WhiteCircle = @@_Load(RESOURCE_General__WhiteCircle_png);
 
 // ★ここまで固定 -- 持ち回り_共通 -- サンプルとしてキープ
 
+var<Picture_t> P_GameStartButton = @@_Load(RESOURCE_Picture__GameStartButton_png);
 var<Picture_t> P_ExplodePiece = @@_Load(RESOURCE_Picture__光る星20_png);
 var<Picture_t> P_Goal = @@_Load(RESOURCE_Picture__Goal_png);
 
