93fe6fdb92013d99acb90602f89a0889d38e1f62
soleil.taruto (cerulean.charlotte) <soleil.taruto@gmail.com>
2022-08-16 19:47:24 +0900

    ss

--- Dev/GameJS/00_Cirno/Gattonero20200001/GameCommons/Keyboard.js
+++ Dev/GameJS/00_Cirno/Gattonero20200001/GameCommons/Keyboard.js
@@ -17,6 +17,11 @@ var @@_Status = {};
 function <void> @@_A_KeyDown(e)
 {
 	@@_Status["" + e.keyCode] = true;
+
+	if (37 <= e.keyCode && e.keyCode <= 40) // ? カーソルキー -> 画面スクロール阻止のため動作キャンセルする。
+	{
+		e.preventDefault();
+	}
 }
 
 function <void> @@_A_KeyUp(e)
