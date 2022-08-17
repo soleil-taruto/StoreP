e6a04d5a89950369b6ecfd13ea344bba4bd391a6
soleil.taruto (cerulean.charlotte) <soleil.taruto@gmail.com>
2022-08-17 10:16:49 +0900

    ss

---
+++ Dev/Program/EditPicture/Claes20200001/Claes20200001/Common.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Charlotte.Commons;
+
+namespace Charlotte
+{
+	public static class Common
+	{
+		public static bool IsSame(I3Color a, I3Color b)
+		{
+			return
+				a.R == b.R &&
+				a.G == b.G &&
+				a.B == b.B;
+		}
+
+		public static bool IsSame(I4Color a, I4Color b)
+		{
+			return
+				a.R == b.R &&
+				a.G == b.G &&
+				a.B == b.B &&
+				a.A == b.A;
+		}
+	}
+}
---
+++ Dev/Program/EditPicture/Claes20200001/Claes20200001/Consts.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Charlotte
+{
+	public static class Consts
+	{
+		/// <summary>
+		/// 画像ファイルの拡張子
+		/// </summary>
+		public static string[] IMAGE_EXTS = new string[] 
+		{
+			".bmp",
+			".gif", 
+			".jpg",
+			".jpeg",
+			".png",
+		};
+	}
+}
