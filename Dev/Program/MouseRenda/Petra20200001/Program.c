64f86d7a800c280e5281b096d15ed679fdaf32c4
soleil.taruto (cerulean.charlotte) <soleil.taruto@gmail.com>
2022-08-28 07:46:50 +0900

    ss

--- Dev/Program/MouseRenda/Petra20200001/Program.c
+++ Dev/Program/MouseRenda/Petra20200001/Program.c
@@ -1,81 +1,188 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+#pragma comment(lib, "user32.lib")
+
 #include <windows.h>
+#include <stdio.h>
+#include <math.h>
 
-#define APP_IDENT "{a361e0aa-7fa4-4d68-85f5-0cb8ffee4c5a}" // アプリ毎に変更する。
-
-#define STOP_EV_NAME APP_IDENT "_STOP_CRON"
-#define PERIOD_SEC 60
-
-static time_t CurrTime;
-
-static void RunIfTimeout(char *command, int periodSec)
+static void Error(void)
 {
-	int remSec = (int)(CurrTime % periodSec);
-
-	printf("Command: %s (%d, %d, %d)\n", command, periodSec, remSec, periodSec - remSec);
-
-	if (remSec == 0)
-		system(command);
-
-	printf("Command_End\n");
+	printf("ERROR!\n");
+	exit(1);
 }
-static void RunIfTimeoutAll(void)
+
+static int DoubleToInt(double value)
 {
-	printf("----\n");
-	printf("%s", ctime(&CurrTime));
-
-	// 周期は PERIOD_SEC の倍数であること。
-
-	RunIfTimeout(".\\Cron1m.bat", 60);
-	RunIfTimeout(".\\Cron3m.bat", 60 * 3);
-	RunIfTimeout(".\\Cron10m.bat", 60 * 10);
-	RunIfTimeout(".\\Cron30m.bat", 60 * 30);
-	RunIfTimeout(".\\Cron1h.bat", 3600);
-	RunIfTimeout(".\\Cron3h.bat", 3600 * 3);
-	RunIfTimeout(".\\Cron10h.bat", 3600 * 10);
-	RunIfTimeout(".\\Cron30h.bat", 3600 * 30);
+	if (value < 0.0)
+	{
+		return (int)(value - 0.5);
+	}
+	else
+	{
+		return (int)(value + 0.5);
+	}
 }
-void main(int argc, char **argv)
+
+static void DoMouseCursor(int x, int y)
 {
-	HANDLE evStop = CreateEventA(NULL, 0, 0, STOP_EV_NAME);
-	int waitSec;
+	INPUT i = { 0 };
 
-	if (evStop == NULL)
+	i.type = INPUT_MOUSE;
+	i.mi.dx = DoubleToInt(x * (65536.0 / GetSystemMetrics(SM_CXSCREEN)));
+	i.mi.dy = DoubleToInt(y * (65536.0 / GetSystemMetrics(SM_CYSCREEN)));
+	i.mi.dwFlags = MOUSEEVENTF_ABSOLUTE | MOUSEEVENTF_MOVE;
+	i.mi.dwExtraInfo = GetMessageExtraInfo();
+
+	SendInput(1, &i, sizeof(INPUT));
+}
+
+/*
+	button:
+		1 == left button
+		2 == middle button
+		3 == right button
+
+	down:
+		TRUE == down
+		FALSE == up
+*/
+static void DoMouseButton(int button, int down)
+{
+	int flag;
+	INPUT i = { 0 };
+
+	switch (button)
 	{
-		printf("Error: CreateEventA()\n");
-		exit(1);
+	case 1: flag = down ? MOUSEEVENTF_LEFTDOWN   : MOUSEEVENTF_LEFTUP;   break;
+	case 2: flag = down ? MOUSEEVENTF_MIDDLEDOWN : MOUSEEVENTF_MIDDLEUP; break;
+	case 3: flag = down ? MOUSEEVENTF_RIGHTDOWN  : MOUSEEVENTF_RIGHTUP;  break;
+
+	default:
+		Error();
 	}
 
-	if (argc == 2 && !_stricmp(argv[1], "/S"))
+	i.type = INPUT_MOUSE;
+	i.mi.dwFlags = flag;
+	i.mi.dwExtraInfo = GetMessageExtraInfo();
+
+	SendInput(1, &i, sizeof(INPUT));
+}
+
+/*
+	level:
+		負の値 == 手前に転がす
+		正の値 == 奥へ転がす
+*/
+static void DoMouseWheel(int level)
+{
+	INPUT i = { 0 };
+
+	i.type = INPUT_MOUSE;
+	i.mi.mouseData = level * WHEEL_DELTA;
+	i.mi.dwFlags = MOUSEEVENTF_WHEEL;
+	i.mi.dwExtraInfo = GetMessageExtraInfo();
+
+	SendInput(1, &i, sizeof(INPUT));
+}
+
+static char Keys[0x100]; // 最後のキー押下状態マップ
+
+static void CheckKeyEach(void)
+{
+	int vk;
+
+	for (vk = 0; vk <= 0xff; vk++)
 	{
-		SetEvent(evStop);
-		printf("SetEvent_Stop\n");
-		goto endProc;
+		Keys[vk] = GetAsyncKeyState(vk) ? 1 : 0;
+	}
+}
+
+static int IsPressKey(unsigned __int16 vk)
+{
+	return Keys[vk] != 0;
+}
+
+static int IsPressLShift(void)
+{
+	return IsPressKey(VK_LSHIFT);
+}
+
+static int IsPressLControl(void)
+{
+	return IsPressKey(VK_LCONTROL);
+}
+
+static int IsPressRShift(void)
+{
+	return IsPressKey(VK_RSHIFT);
+}
+
+static int IsPressRControl(void)
+{
+	return IsPressKey(VK_RCONTROL);
+}
+
+static void DoKeyboard(unsigned __int16 vk, int down)
+{
+	INPUT i = { 0 };
+
+	i.type = INPUT_KEYBOARD;
+	i.ki.wVk = vk;
+	i.ki.wScan = MapVirtualKey(vk, 0);
+	i.ki.dwFlags = KEYEVENTF_EXTENDEDKEY | (down ? 0 : KEYEVENTF_KEYUP);
+	i.mi.dwExtraInfo = GetMessageExtraInfo();
+
+	SendInput(1, &i, sizeof(INPUT));
+}
+
+main()
+{
+	int loopCount;
+	int lBtnPressed = 0;
+	int rBtnPressed = 0;
+	int lBtnPress;
+	int rBtnPress;
+
+	printf("+---------------------------------+\n");
+	printf("| 右シフト --------> 左ボタン連打 |\n");
+	printf("| 右コントロール --> 右ボタン連打 |\n");
+	printf("| 左シフトとコントロール --> 終了 |\n");
+	printf("+---------------------------------+\n");
+	printf("START...\n");
+
+	for (loopCount = 0; ; loopCount++)
+	{
+		Sleep(20); // 16.666 (60 Hz) より長くしておく。
+
+		CheckKeyEach();
+
+		if (IsPressLShift() && IsPressLControl())
+		{
+			break;
+		}
+
+		lBtnPress = IsPressRShift()   ? loopCount % 2 == 0 : 0;
+		rBtnPress = IsPressRControl() ? loopCount % 2 == 1 : 0;
+
+		if (lBtnPressed ? !lBtnPress : lBtnPress)
+		{
+			DoMouseButton(1, lBtnPress);
+			lBtnPressed = lBtnPress;
+		}
+		if (rBtnPressed ? !rBtnPress : rBtnPress)
+		{
+			DoMouseButton(3, rBtnPress);
+			rBtnPressed = rBtnPress;
+		}
 	}
 
-	printf("ST_Cron\n");
-
-	CurrTime = 0;
-	RunIfTimeoutAll();
-	CurrTime = (time(NULL) / PERIOD_SEC) * PERIOD_SEC;
-
-	do
+	if (lBtnPressed)
 	{
-		RunIfTimeoutAll();
-		CurrTime += PERIOD_SEC;
-
-		waitSec = CurrTime - time(NULL);
-		waitSec = __max(waitSec, PERIOD_SEC - PERIOD_SEC / 2);
-		waitSec = __min(waitSec, PERIOD_SEC + PERIOD_SEC / 2);
-
-		printf("waitSec: %d\n", waitSec);
+		DoMouseButton(1, 0);
 	}
-	while (WaitForSingleObject(evStop, waitSec * 1000) == WAIT_TIMEOUT);
+	if (rBtnPressed)
+	{
+		DoMouseButton(3, 0);
+	}
 
-	printf("ED_Cron\n");
-
-endProc:
-	CloseHandle(evStop);
+	printf("END\n");
 }
