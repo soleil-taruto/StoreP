efadc191ae2c72d65e165c9a054d0bbee06859ca
soleil.taruto (cerulean.charlotte) <soleil.taruto@gmail.com>
2022-09-04 14:54:44 +0900

    ss

--- Dev/Program/Test0001/Claes20200001/Claes20200001/Tests/Test0001.cs
+++ Dev/Program/Test0001/Claes20200001/Claes20200001/Tests/Test0001.cs
@@ -11,7 +11,65 @@ namespace Charlotte.Tests
 	{
 		public void Test01()
 		{
-			// none
+			Test01_a(100, 30000);
+			Test01_a(300, 10000);
+			Test01_a(1000, 3000);
+			Test01_a(3000, 1000);
+			Test01_a(10000, 300);
+			Test01_a(30000, 100);
+
+			Console.WriteLine("OK!");
+		}
+
+		private void Test01_a(int dataSizeLmt, int testCount)
+		{
+			for (int testcnt = 0; testcnt < testCount; testcnt++)
+			{
+				byte[] src = SCommon.CRandom.GetBytes(SCommon.CRandom.GetInt(dataSizeLmt));
+				string enc = SCommon.Base64.I.Encode(src);
+				byte[] dec = SCommon.Base64.I.Decode(enc);
+
+				if (SCommon.Comp(src, dec) != 0) // ? 不一致
+					throw null;
+			}
+			Console.WriteLine("OK");
+		}
+
+		public void Test02()
+		{
+			Test02_a(100, 30000);
+			Test02_a(300, 10000);
+			Test02_a(1000, 3000);
+			Test02_a(3000, 1000);
+			Test02_a(10000, 300);
+			Test02_a(30000, 100);
+
+			Console.WriteLine("OK!");
+		}
+
+		private void Test02_a(int dataSizeLmt, int testCount)
+		{
+			for (int testcnt = 0; testcnt < testCount; testcnt++)
+			{
+				byte[] src = SCommon.CRandom.GetBytes(SCommon.CRandom.GetInt(dataSizeLmt));
+				string enc = SCommon.Base64.I.Encode(src);
+
+				// Change enc
+				{
+					enc = enc.Replace("=", "");
+
+					for (int c = 80; c + 30 < enc.Length; c += 100)
+					{
+						enc = enc.Substring(0, c) + "\r\n\t" + enc.Substring(c);
+					}
+				}
+
+				byte[] dec = SCommon.Base64.I.Decode(enc);
+
+				if (SCommon.Comp(src, dec) != 0) // ? 不一致
+					throw null;
+			}
+			Console.WriteLine("OK");
 		}
 	}
 }
