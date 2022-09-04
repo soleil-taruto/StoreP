aadeb89e3eeef9660715f37ac2c8e7672ffd2337
soleil.taruto (cerulean.charlotte) <soleil.taruto@gmail.com>
2022-09-05 07:24:07 +0900

    ss

--- Dev/Program/00_Utilities/Claes20200001/Claes20200001/Tests/Test0008.cs
+++ Dev/Program/00_Utilities/Claes20200001/Claes20200001/Tests/Test0008.cs
@@ -8,37 +8,111 @@ using Charlotte.Utilities;
 namespace Charlotte.Tests
 {
 	/// <summary>
-	/// EditableString.cs テスト
+	/// StringSpliceSequencer.cs テスト
 	/// </summary>
 	public class Test0008
 	{
 		public void Test01()
 		{
-			EditableString es = new EditableString("ABC");
+			for (int testcnt = 0; testcnt < 10000; testcnt++)
+			{
+				Test01_a();
+			}
+			Console.WriteLine("OK!");
+		}
 
-			Console.WriteLine(es);
-			es.Add('D');
-			Console.WriteLine(es);
-			es.Add("EF");
-			Console.WriteLine(es);
-			es.Insert(3, "123");
-			Console.WriteLine(es);
-			es.Insert(0, '(');
-			Console.WriteLine(es);
-			es.Insert(10, ')');
-			Console.WriteLine(es);
-			es.Remove(4);
-			Console.WriteLine(es);
-			es.Remove(4, 2);
-			Console.WriteLine(es);
-			es.Remove(0);
-			Console.WriteLine(es);
-			es.Remove(6);
-			Console.WriteLine(es);
+		private void Test01_a()
+		{
+			string[] words = new string[]
+			{
+				"Sio",
+				"Miso",
+				"Syoyu",
+				"Hakata",
+				"Sapporo",
+				"Tonkotsu",
+			};
 
-			SCommon.ToThrowPrint(() => es.Insert(7, "xxx"));
-			SCommon.ToThrowPrint(() => es.Remove(6));
-			SCommon.ToThrowPrint(() => es.Remove(5, 2));
+			string src = string.Join("", Enumerable.Range(0, SCommon.CRandom.GetInt(100)).Select(dummy => SCommon.CRandom.ChooseOne(words)));
+			string word1 = SCommon.CRandom.ChooseOne(words);
+			string word2 = SCommon.CRandom.ChooseOne(words);
+
+			string ans1 = src.Replace(word1, word2);
+			string ans2;
+
+			{
+				StringSpliceSequencer sss = new StringSpliceSequencer(src);
+				int index = 0;
+
+				for (; ; )
+				{
+					index = src.IndexOf(word1, index);
+
+					if (index == -1)
+						break;
+
+					sss.Splice(index, word1.Length, word2);
+					index += word1.Length;
+				}
+				ans2 = sss.GetString();
+			}
+
+			if (ans1 != ans2) // ? 不一致
+				throw null;
+		}
+
+		public void Test02()
+		{
+			for (int testcnt = 0; testcnt < 10000; testcnt++)
+			{
+				Test02_a();
+			}
+			Console.WriteLine("OK!");
+		}
+
+		private void Test02_a()
+		{
+			char[] SRC_CHARS = SCommon.HALF.ToArray();
+			string src = new string(Enumerable
+				.Range(0, SCommon.CRandom.GetInt(100))
+				.Select(dummy => SCommon.CRandom.ChooseOne(SRC_CHARS))
+				.ToArray());
+
+			string ans1 = src;
+			string ans2;
+
+			int index1 = 0;
+			int index2 = 0;
+
+			StringSpliceSequencer sss = new StringSpliceSequencer(src);
+
+			for (; ; )
+			{
+				int span = SCommon.CRandom.GetInt(30);
+
+				index1 += span;
+				index2 += span;
+
+				int removeLength = SCommon.CRandom.GetInt(30);
+
+				if (ans1.Length < index1 + removeLength)
+					break;
+
+				string newPart = new string(Enumerable
+					.Range(0, SCommon.CRandom.GetInt(30))
+					.Select(dummy => SCommon.CRandom.ChooseOne(SRC_CHARS))
+					.ToArray());
+
+				ans1 = ans1.Substring(0, index1) + newPart + ans1.Substring(index1 + removeLength);
+				sss.Splice(index2, removeLength, newPart);
+
+				index1 += newPart.Length;
+				index2 += removeLength;
+			}
+			ans2 = sss.GetString();
+
+			if (ans1 != ans2) // ? 不一致
+				throw null;
 		}
 	}
 }
