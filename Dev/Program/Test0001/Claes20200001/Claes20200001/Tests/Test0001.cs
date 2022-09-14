6028efa359c742d4c2605f4c0c95a3575cf993c7
soleil.taruto (cerulean.charlotte) <soleil.taruto@gmail.com>
2022-09-14 13:26:08 +0900

    ss

--- Dev/Program/Test0001/Claes20200001/Claes20200001/Tests/Test0001.cs
+++ Dev/Program/Test0001/Claes20200001/Claes20200001/Tests/Test0001.cs
@@ -11,7 +11,91 @@ namespace Charlotte.Tests
 	{
 		public void Test01()
 		{
-			// none
+			//Test01_a(100000, 20, 1000);
+			//Test01_a(2000, 2000, 1000000);
+			//Test01_a(30, 200000, 1000000000);
+
+			Test01_a(100000, 30, 30);
+			Test01_a(10000, 200, 200);
+			Test01_a(1000, 1000, 1000);
+			Test01_a(200, 2000, 2000);
+			Test01_a(30, 3000, 3000);
+
+			Console.WriteLine("OK!");
+		}
+
+		private void Test01_a(int testCount, int nMax, int aMax)
+		{
+			for (int testcnt = 0; testcnt < testCount; testcnt++)
+			{
+				int[] aArr = Enumerable.Range(0, SCommon.CRandom.GetRange(2, nMax)).Select(dummy => SCommon.CRandom.GetRange(0, aMax)).ToArray();
+
+				int ans1 = Test01_b1(aArr);
+				int ans2 = Test01_b2(aArr);
+
+				if (ans1 != ans2)
+					throw null;
+			}
+			Console.WriteLine("OK");
+		}
+
+		private int Test01_b1(int[] aArr)
+		{
+			int aMin = aArr.Min();
+
+			Queue<int> q = new Queue<int>(aArr.Select(a => a - aMin).Where(a => 1 <= a));
+
+			if (q.Count == 0)
+				return 1;
+
+			while (2 <= q.Count)
+			{
+				int a = q.Dequeue();
+				int b = q.Dequeue();
+
+				int gcd = GetGCD(a, b);
+
+				q.Enqueue(gcd);
+			}
+
+			{
+				int gcd = q.Dequeue();
+
+				return 2 <= gcd ? 1 : 2;
+			}
+		}
+
+		private int GetGCD(int a, int b)
+		{
+			if (b == 0)
+				return a;
+
+			return GetGCD(b, a % b);
+		}
+
+		private int Test01_b2(int[] aArr)
+		{
+			int aMax = aArr.Max();
+			int kindMin = int.MaxValue;
+
+			for (int m = 2; m <= Math.Max(2, aMax + 1); m++)
+			{
+				bool[] modMap = new bool[aMax + 1];
+				int kind = 0;
+
+				foreach (int a in aArr)
+				{
+					int mod = a % m;
+
+					if (!modMap[mod])
+					{
+						modMap[mod] = true;
+						kind++;
+					}
+				}
+				kindMin = Math.Min(kindMin, kind);
+			}
+			return kindMin;
 		}
 	}
 }
