91c36aac372fe0934ccf5d591bd47d39521b83d6
soleil.taruto (cerulean.charlotte) <soleil.taruto@gmail.com>
2022-09-17 11:48:21 +0900

    ss

--- Dev/Program/Test0001/Claes20200001/Claes20200001/Tests/Test0003.cs
+++ Dev/Program/Test0001/Claes20200001/Claes20200001/Tests/Test0003.cs
@@ -11,10 +11,8 @@ namespace Charlotte.Tests
 		public void Test01()
 		{
 			Test01_a(1000, 10);
-			Test01_a(300, 30);
-			Test01_a(100, 100);
-			Test01_a(30, 300);
-			Test01_a(10, 1000);
+			Test01_a(100, 30);
+			Test01_a(10, 100);
 
 			Console.WriteLine("OK!");
 		}
@@ -31,6 +29,8 @@ namespace Charlotte.Tests
 
 		private void Test01_a2(int testCount, int cardCountMax, int cardValueMax)
 		{
+			Console.WriteLine(string.Join(", ", testCount, cardCountMax, cardValueMax));
+
 			for (int testcnt = 0; testcnt < testCount; testcnt++)
 			{
 				int[] cards = Enumerable
@@ -58,39 +58,56 @@ namespace Charlotte.Tests
 				.Select(card => card.ToString())
 				.ToArray();
 
-			Array.Sort(sCards, SCommon.Comp);
+			long best = -1;
 
-			return long.Parse(string.Join("", sCards.Reverse())); // BUG
+			ChooseDifferentThree(3, (index1, index2, index3) =>
+			{
+				long value = long.Parse(string.Join("", sCards[index1], sCards[index2], sCards[index3]));
+
+				if (best < value)
+					best = value;
+			});
+
+			return best;
 		}
 
 		private long Test01_b2(int[] cards)
 		{
 			long best = -1;
 
-			for (int index1 = 0; index1 < cards.Length; index1++)
+			ChooseDifferentThree(cards.Length, (index1, index2, index3) =>
 			{
-				for (int index2 = 0; index2 < cards.Length; index2++)
+				string str = string.Format("{0}{1}{2}", cards[index1], cards[index2], cards[index3]);
+				long value = long.Parse(str);
+
+				if (best < value)
+					best = value;
+			});
+
+			return best;
+		}
+
+		private void ChooseDifferentThree(int count, Action<int, int, int> routine)
+		{
+			for (int index1 = 0; index1 < count; index1++)
+			{
+				for (int index2 = 0; index2 < count; index2++)
 				{
 					if (index1 != index2)
 					{
-						for (int index3 = 0; index3 < cards.Length; index3++)
+						for (int index3 = 0; index3 < count; index3++)
 						{
 							if (
 								index1 != index3 &&
 								index2 != index3
 								)
 							{
-								string str = string.Format("{0}{1}{2}", cards[index1], cards[index2], cards[index3]);
-								long value = long.Parse(str);
-
-								if (best < value)
-									best = value;
+								routine(index1, index2, index3);
 							}
 						}
 					}
 				}
 			}
-			return best;
 		}
 	}
 }
