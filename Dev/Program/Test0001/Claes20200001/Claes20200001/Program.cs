e4028746ac259aca3bf41ba5c8421bb4a7cadf06
soleil.taruto (cerulean.charlotte) <soleil.taruto@gmail.com>
2022-09-17 22:29:19 +0900

    ss

--- Dev/Program/Test0001/Claes20200001/Claes20200001/Program.cs
+++ Dev/Program/Test0001/Claes20200001/Claes20200001/Program.cs
@@ -61,7 +61,9 @@ namespace Charlotte
 			//new Test0003().Test01();
 			//new Test0004().Test01();
 			//new Test0005().Test01();
-			new Test0005().Test02();
+			//new Test0005().Test02();
+			//new Test0005().Test03();
+			new Test0005().Test04();
 
 			// --
 		}
--- Dev/Program/Test0001/Claes20200001/Claes20200001/Tests/Test0005.cs
+++ Dev/Program/Test0001/Claes20200001/Claes20200001/Tests/Test0005.cs
@@ -116,12 +116,86 @@ namespace Charlotte.Tests
 
 		private ulong Test03_b1(ulong a, ulong b)
 		{
-			throw new NotImplementedException();
+			b ^= ulong.MaxValue;
+			unchecked { b++; }
+
+			while (b != 0)
+			{
+				ulong na = a ^ b;
+				b = (a & b) << 1;
+				a = na;
+			}
+			return a;
 		}
 
 		private ulong Test03_b2(ulong a, ulong b)
 		{
-			throw new NotImplementedException();
+			unchecked
+			{
+				return a - b;
+			}
+		}
+
+		/// <summary>
+		/// 割り算
+		/// </summary>
+		public void Test04()
+		{
+			Test04_a(ulong.MaxValue);
+			Test04_a(0x0000ffffffffffffUL);
+			Test04_a(0x00000000ffffffffUL);
+			Test04_a(0x000000000000ffffUL);
+
+			Console.WriteLine("OK!");
+		}
+
+		private void Test04_a(ulong m)
+		{
+			for (int testcnt = 0; testcnt < 1000000; testcnt++)
+			{
+				ulong a = SCommon.CRandom.GetULong();
+				ulong b = SCommon.CRandom.GetULong();
+
+				b &= m;
+
+				if (b == 0)
+					continue;
+
+				ulong ans1 = Test04_b1(a, b);
+				ulong ans2 = Test04_b2(a, b);
+
+				if (ans1 != ans2)
+					throw null;
+			}
+			Console.WriteLine("OK");
+		}
+
+		private ulong Test04_b1(ulong a, ulong b)
+		{
+			ulong d = 1;
+			ulong n = 0;
+
+			while ((b & (1UL << 63)) == 0)
+			{
+				b <<= 1;
+				d <<= 1;
+			}
+			while (d != 0)
+			{
+				if (a >= b)
+				{
+					a -= b;
+					n |= d;
+				}
+				b >>= 1;
+				d >>= 1;
+			}
+			return n;
+		}
+
+		private ulong Test04_b2(ulong a, ulong b)
+		{
+			return a / b;
 		}
 	}
 }
