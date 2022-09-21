53305ab42e7ff9a4df01dcd2cb840598c064089d
soleil.taruto (cerulean.charlotte) <soleil.taruto@gmail.com>
2022-09-21 12:09:38 +0900

    ss

---
+++ Dev/Program/Test0001/Claes20200001/Claes20200001/Tests/Test0002.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Charlotte.Utilities;
+
+namespace Charlotte.Tests
+{
+	public class Test0002
+	{
+		public void Test01()
+		{
+			for (ulong n = 1; n < ulong.MaxValue; n = n * 2 + 1)
+			{
+				if (MillerRabin.IsPrime(n))
+				{
+					Console.WriteLine(n.ToString("x16") + " " + n);
+				}
+			}
+		}
+	}
+}
