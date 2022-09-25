6d7547d48f7870f25fe18328dd5c5dc3d8aa128c
soleil.taruto (cerulean.charlotte) <soleil.taruto@gmail.com>
2022-09-26 00:09:17 +0900

    ss

---
+++ Dev/Game/00_Shooting/Elsa20200001/Elsa20200001/Games/Enemies/EnemyCommon.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Charlotte.Commons;
+using Charlotte.GameCommons;
+
+namespace Charlotte.Games.Enemies
+{
+	public static class EnemyCommon
+	{
+		/// <summary>
+		/// 汎用・被弾イベント
+		/// </summary>
+		/// <param name="enemy"></param>
+		public static void Damaged(Enemy enemy)
+		{
+			// TODO: SE
+		}
+
+		/// <summary>
+		/// 汎用・死亡イベント
+		/// </summary>
+		public static void Killed(Enemy enemy)
+		{
+			DDGround.EL.Add(SCommon.Supplier(Effects.中爆発(enemy.X, enemy.Y)));
+		}
+	}
+}
