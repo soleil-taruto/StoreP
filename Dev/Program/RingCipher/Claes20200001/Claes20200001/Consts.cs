f7bcd41e50e77bf947400e087e43b2f240246129
soleil.taruto (cerulean.charlotte) <soleil.taruto@gmail.com>
2022-09-28 02:52:11 +0900

    ss

---
+++ Dev/Program/RingCipher/Claes20200001/Claes20200001/Consts.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Charlotte
+{
+	public static class Consts
+	{
+		/// <summary>
+		/// ファイル処理をメモリ上で行うファイルサイズの最大値
+		/// 単位：バイト
+		/// </summary>
+		public const long ON_MEMORY_FILE_SIZE_MAX = 202000000; // 192 * 1024 ^ 2 == 201326592
+	}
+}
