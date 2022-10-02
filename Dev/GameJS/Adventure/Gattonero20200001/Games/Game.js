ada8f1c1d8ac6997626cd30d143e70b8095fde5a
soleil.taruto (cerulean.charlotte) <soleil.taruto@gmail.com>
2022-10-03 05:26:16 +0900

    ss

---
+++ Dev/GameJS/Adventure/Gattonero20200001/Games/Game.js
@@ -0,0 +1,41 @@
+/*
+	ゲーム・メイン
+*/
+
+// カメラ位置(整数)
+var<D2Point_t> Camera = CreateD2Point(0.0, 0.0);
+
+// ゲーム用タスク
+var<TaskManager_t> GameTasks = CreateTaskManager();
+
+function* <generatorForTask> GameMain()
+{
+	FreezeInput();
+	ClearAllActor();
+
+	var<Actor_t> card = CreateActor_Trump(Screen_W / 2.0, Screen_H / 2.0, 1, 1, false);
+	var<boolean> reversed = false;
+
+	AddActor(card);
+
+	for (; ; )
+	{
+		ClearScreen();
+
+		if (GetInput_Pause() == 1)
+		{
+			break;
+		}
+
+		if (GetMouseDown() == -1)
+		{
+			reversed = !reversed;
+			SetTrumpReversed(card, reversed);
+		}
+
+		ExecuteAllActor();
+
+		yield 1;
+	}
+	FreezeInput();
+}
