5737e0ad11a662e58c34e1891874255f5d2e3de8
soleil.taruto (cerulean.charlotte) <soleil.taruto@gmail.com>
2022-10-07 03:47:37 +0900

    ss

--- Dev/Program/Test0001/Claes20200001/Claes20200001/Tests/Test0013.cs
+++ Dev/Program/Test0001/Claes20200001/Claes20200001/Tests/Test0013.cs
@@ -13,21 +13,67 @@ namespace Charlotte.Tests
 		{
 			char[] allChrs = SCommon.GetJCharCodes()
 				.Select(v => new byte[] { (byte)(v >> 8), (byte)(v & 0xff) })
-				.Where(v => SCommon.Comp(v, SCommon.ENCODING_SJIS.GetBytes(SCommon.ENCODING_SJIS.GetString(v))) == 0)
+				//.Where(v => SCommon.Comp(v, SCommon.ENCODING_SJIS.GetBytes(SCommon.ENCODING_SJIS.GetString(v))) == 0)
 				.Select(v => SCommon.ENCODING_SJIS.GetString(v)[0])
 				.ToArray();
 
 			char[][] chrLChrPairs = allChrs.Select(v => new char[] { v, char.ToLower(v) }).Where(v => v[0] != v[1]).ToArray();
 			char[][] uChrChrPairs = allChrs.Select(v => new char[] { char.ToUpper(v), v }).Where(v => v[0] != v[1]).ToArray();
 
-			Array.Sort(chrLChrPairs, (a, b) => (int)a[0] - (int)b[0]);
-			Array.Sort(uChrChrPairs, (a, b) => (int)a[0] - (int)b[0]);
+			List<char[]> chrLChrPairOnly = new List<char[]>();
+			List<char[]> chrLChrPairBoth = new List<char[]>();
+			List<char[]> uChrChrPairBoth = new List<char[]>();
+			List<char[]> uChrChrPairOnly = new List<char[]>();
 
-			File.WriteAllLines(@"C:\temp\1.txt", chrLChrPairs.Select(v => v[0] + ", " + v[1]), Encoding.UTF8);
-			File.WriteAllLines(@"C:\temp\2.txt", uChrChrPairs.Select(v => v[0] + ", " + v[1]), Encoding.UTF8);
+			SCommon.Merge(
+				chrLChrPairs,
+				uChrChrPairs,
+				(a, b) => SCommon.Comp(a, b, (aa, bb) => (int)aa - (int)bb),
+				chrLChrPairOnly,
+				chrLChrPairBoth,
+				uChrChrPairBoth,
+				uChrChrPairOnly
+				);
 
-			if (SCommon.Comp(chrLChrPairs, uChrChrPairs, (a, b) => SCommon.Comp(a, b, (aa, bb) => (int)aa - (int)bb)) != 0)
-				throw null;
+			foreach (char[] pair in chrLChrPairOnly)
+				Console.WriteLine("chrLChrPairOnly: [" + pair[0] + "], [" + pair[1] + "]");
+
+			foreach (char[] pair in uChrChrPairOnly)
+				Console.WriteLine("uChrChrPairOnly: [" + pair[0] + "], [" + pair[1] + "]");
+
+			string WORK_DIR = @"C:\temp\Test0013_WorkDir";
+
+			foreach (char[] pair in chrLChrPairBoth)
+			{
+				string path1 = Path.Combine(WORK_DIR, new string(new char[] { pair[0] }));
+				string path2 = Path.Combine(WORK_DIR, new string(new char[] { pair[1] }));
+
+				Console.WriteLine("*1 " + path1);
+				Console.WriteLine("*2 " + path2);
+
+				// ----
+
+				SCommon.DeletePath(WORK_DIR);
+				SCommon.CreateDir(WORK_DIR);
+
+				File.WriteAllBytes(path1, SCommon.EMPTY_BYTES);
+
+				if (!File.Exists(path2))
+					throw null;
+
+				// ----
+
+				SCommon.DeletePath(WORK_DIR);
+				SCommon.CreateDir(WORK_DIR);
+
+				File.WriteAllBytes(path2, SCommon.EMPTY_BYTES);
+
+				if (!File.Exists(path1))
+					throw null;
+
+				// ----
+			}
+			Console.WriteLine("OK!");
 		}
 	}
 }
