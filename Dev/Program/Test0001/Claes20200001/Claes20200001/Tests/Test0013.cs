65ab8f46d037efbf64ebab3cfa39c836801827d7
soleil.taruto (cerulean.charlotte) <soleil.taruto@gmail.com>
2022-10-07 03:10:02 +0900

    ss

--- Dev/Program/Test0001/Claes20200001/Claes20200001/Tests/Test0013.cs
+++ Dev/Program/Test0001/Claes20200001/Claes20200001/Tests/Test0013.cs
@@ -11,15 +11,13 @@ namespace Charlotte.Tests
 	{
 		public void Test01()
 		{
-			char[] allChrs = Enumerable.Range(0, 65536).Select(v => (char)v).ToArray();
+			char[] allChrs = SCommon.GetJChars().ToArray();
 
 			char[][] chrLChrPairs = allChrs.Select(v => new char[] { v, char.ToLower(v) }).Where(v => v[0] != v[1]).ToArray();
 			char[][] uChrChrPairs = allChrs.Select(v => new char[] { char.ToUpper(v), v }).Where(v => v[0] != v[1]).ToArray();
 
+			Array.Sort(chrLChrPairs, (a, b) => (int)a[0] - (int)b[0]);
 			Array.Sort(uChrChrPairs, (a, b) => (int)a[0] - (int)b[0]);
-
-			File.WriteAllLines(@"C:\temp\1.txt", chrLChrPairs.Select(v => v[0] + ", " + v[1]), Encoding.UTF8);
-			File.WriteAllLines(@"C:\temp\2.txt", uChrChrPairs.Select(v => v[0] + ", " + v[1]), Encoding.UTF8);
 
 			if (SCommon.Comp(chrLChrPairs, uChrChrPairs, (a, b) => SCommon.Comp(a, b, (aa, bb) => (int)aa - (int)bb)) != 0)
 				throw null;
