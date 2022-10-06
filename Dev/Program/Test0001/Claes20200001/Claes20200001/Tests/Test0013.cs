26380e9bc629a70c22c7d2dd008b1832aa537077
soleil.taruto (cerulean.charlotte) <soleil.taruto@gmail.com>
2022-10-07 03:32:30 +0900

    ss

--- Dev/Program/Test0001/Claes20200001/Claes20200001/Tests/Test0013.cs
+++ Dev/Program/Test0001/Claes20200001/Claes20200001/Tests/Test0013.cs
@@ -11,13 +11,20 @@ namespace Charlotte.Tests
 	{
 		public void Test01()
 		{
-			char[] allChrs = SCommon.GetJChars().ToArray();
+			char[] allChrs = SCommon.GetJCharCodes()
+				.Select(v => new byte[] { (byte)(v >> 8), (byte)(v & 0xff) })
+				.Where(v => SCommon.Comp(v, SCommon.ENCODING_SJIS.GetBytes(SCommon.ENCODING_SJIS.GetString(v))) == 0)
+				.Select(v => SCommon.ENCODING_SJIS.GetString(v)[0])
+				.ToArray();
 
 			char[][] chrLChrPairs = allChrs.Select(v => new char[] { v, char.ToLower(v) }).Where(v => v[0] != v[1]).ToArray();
 			char[][] uChrChrPairs = allChrs.Select(v => new char[] { char.ToUpper(v), v }).Where(v => v[0] != v[1]).ToArray();
 
 			Array.Sort(chrLChrPairs, (a, b) => (int)a[0] - (int)b[0]);
 			Array.Sort(uChrChrPairs, (a, b) => (int)a[0] - (int)b[0]);
+
+			File.WriteAllLines(@"C:\temp\1.txt", chrLChrPairs.Select(v => v[0] + ", " + v[1]), Encoding.UTF8);
+			File.WriteAllLines(@"C:\temp\2.txt", uChrChrPairs.Select(v => v[0] + ", " + v[1]), Encoding.UTF8);
 
 			if (SCommon.Comp(chrLChrPairs, uChrChrPairs, (a, b) => SCommon.Comp(a, b, (aa, bb) => (int)aa - (int)bb)) != 0)
 				throw null;
