ec53bc8322e86cd297862c66f8a2fa186daf3897
soleil.taruto (cerulean.charlotte) <soleil.taruto@gmail.com>
2022-10-12 21:41:36 +0900

    ss

--- Dev/Program/Test0001/Claes20200001/Claes20200001/Program.cs
+++ Dev/Program/Test0001/Claes20200001/Claes20200001/Program.cs
@@ -60,7 +60,8 @@ namespace Charlotte
 			//new Test0002().Test03();
 			//new Test0002().Test04();
 			//new Test0003().Test01();
-			new Test0004().Test01();
+			//new Test0004().Test01();
+			new Test0005().Test01();
 
 			// --
 		}
---
+++ Dev/Program/Test0001/Claes20200001/Claes20200001/Tests/Test0005.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Charlotte.Commons;
+
+namespace Charlotte.Tests
+{
+	public class Test0005
+	{
+		private const int PLAYER_NUM = 8;
+
+		private class GameInfo
+		{
+			public PlayerInfo[] Players = new PlayerInfo[PLAYER_NUM];
+			public PlayerInfo[] RankOrder;
+		}
+
+		private class PlayerInfo
+		{
+			public int SelfIndex; // 0～
+			public int[] Points = new int[PLAYER_NUM];
+			public int Rank; // 1～
+
+			public int Point
+			{
+				get
+				{
+					return this.Points.Sum();
+				}
+			}
+		}
+
+		private GameInfo CreateGame()
+		{
+			GameInfo game = new GameInfo();
+
+			for (int index = 0; index < PLAYER_NUM; index++)
+			{
+				game.Players[index] = new PlayerInfo();
+				game.Players[index].SelfIndex = index;
+			}
+			for (int b = 1; b < PLAYER_NUM; b++)
+			{
+				for (int a = 0; a < b; a++)
+				{
+					int aPoint = SCommon.CRandom.GetInt(3) * 5;
+					int bPoint = 10 - aPoint;
+
+					game.Players[a].Points[b] = aPoint;
+					game.Players[b].Points[a] = bPoint;
+				}
+			}
+
+			for (int index = 0; index < PLAYER_NUM; index++)
+				game.Players[index].Rank = game.Players.Where(v => game.Players[index].Point < v.Point).Count() + 1;
+
+			game.RankOrder = game.Players.ToArray(); // Cloning
+
+			Array.Sort(game.RankOrder, (a, b) => a.Rank - b.Rank);
+
+			return game;
+		}
+
+		public void Test01()
+		{
+			for (int c = 0; c < 10; )
+			{
+				GameInfo game = CreateGame();
+
+				if (SCommon.HasSame(game.Players, (a, b) => a.Point == b.Point))
+					continue;
+
+				if (
+					game.RankOrder[1].Point !=
+					game.RankOrder[4].Point +
+					game.RankOrder[5].Point +
+					game.RankOrder[6].Point +
+					game.RankOrder[7].Point
+					)
+					continue;
+
+				Console.WriteLine(game.RankOrder[2].Points[game.RankOrder[6].SelfIndex]);
+				c++;
+			}
+		}
+	}
+}
