e7c820a819d59ff2551b961d25a0879215f40470
soleil.taruto (cerulean.charlotte) <soleil.taruto@gmail.com>
2022-10-12 15:42:37 +0900

    ss

---
+++ Dev/Program/Test0001/Claes20200001/Claes20200001/Tests/Test0003.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Charlotte.Commons;
+
+namespace Charlotte.Tests
+{
+	public class Test0003
+	{
+		public void Test01()
+		{
+			using (CsvFileWriter writer = new CsvFileWriter(SCommon.NextOutputPath() + ".csv"))
+			{
+				writer.WriteRow(new string[]
+				{
+					"GetBoolean",
+					"GetSign",
+					"GetReal1",
+					"GetReal2",
+					"GetReal3(100,200)",
+				});
+
+				for (int testcnt = 0; testcnt < 1000; testcnt++)
+				{
+					writer.WriteRow(new string[]
+					{
+						SCommon.CRandom.GetBoolean().ToString(),
+						SCommon.CRandom.GetSign().ToString(),
+						SCommon.CRandom.GetReal1().ToString("F9"),
+						SCommon.CRandom.GetReal2().ToString("F9"),
+						SCommon.CRandom.GetReal3(100, 200).ToString("F9"),
+					});
+				}
+			}
+		}
+	}
+}
